fe6ba4797d8ca3d89d187603a3cfaf1a98453c97
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-02-29 20:02:09 +0900

    working on a theme changer

--- main.js
+++ main.js
@@ -2,16 +2,40 @@ const url = " https://restcountries.com/v3.1/all";
 
 const countriesCards = document.querySelector(".search__results--cards");
 const countryCard = countriesCards.children;
+// const card = document.querySelector(".results--card");
 const select = document.querySelector("select");
 const dropdownOptions = document.querySelectorAll(".option-box");
 const region = document.querySelectorAll(".info-region");
 
+const searchInput = document.querySelector("input");
+
+const btn = document.querySelector(".header__mode-switcher");
+const header = document.querySelector(".header");
+const searchSection = document.querySelector(".search");
+
+let allCountriesData;
+
 //Fetch the data
+// const LoadAPI = function () {
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) =>
+//       data.forEach((data) => {
+//         displayCountries(data);
+//         allCountriesData = data;
+//       })
+//     );
+// };
+
 const LoadAPI = function () {
   fetch(url)
     .then((res) => res.json())
-    .then((data) => data.forEach((data) => displayCountries(data)));
+    .then((data) => {
+      displayCountries(data);
+      allCountriesData = data;
+    });
 };
+
 LoadAPI();
 
 //  Style numbers
@@ -22,11 +46,12 @@ function numberWithCommas(x) {
 //Display all countries
 
 function displayCountries(data) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("results--card");
-  newDiv.innerHTML = `
+  data.forEach((data) => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("results--card");
+    newDiv.innerHTML = `
     <img class="flag-img" src="${data.flags.svg}" />
-      <div class="country-info">
+      <div class="country-info light">
       <span class="country-name">${data.name.common}</span>
       <ul class="country-properies">
         <li class="property-box">
@@ -42,15 +67,35 @@ function displayCountries(data) {
         </li>
       </ul>
   `;
-  countriesCards.appendChild(newDiv);
+    countriesCards.appendChild(newDiv);
+  });
 }
 
 //Display coutries by region
-select.addEventListener("change", displayByRegion);
-function displayByRegion(data) {
-  const regionValue = select.value;
+const byRegion = function () {
+  fetch(`https://restcountries.com/v3.1/region/${select.value}`)
+    .then((res) => res.json())
+    .then((data) => displayCountries(data));
+};
 
-  if (regionValue != "" && regionValue === data.region) {
-    countriesCards.style.display = "block";
-  }
-}
+select.addEventListener("change", byRegion);
+
+// Country by serach input
+
+// searchInput.addEventListener("input", (e) => {
+//   const filteredCountries = allCountriesData.filter((country) =>
+//     country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+//   );
+//   displayCountries(filteredCountries);
+// });
+
+//Theme Switch
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+  header.classList.toggle("light");
+  searchInput.classList.toggle("light");
+  select.classList.toggle("light");
+  countriesCards.classList.toggle("light");
+  searchSection.classList.toggle("light");
+});
