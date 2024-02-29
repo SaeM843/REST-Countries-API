efac5c8bd3d49f0b9d1f908489a6fac12c0367ad
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-02-29 12:31:16 +0900

    displayed all countries from API

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-const url = "https://restcountries.com/v3.1/all";
+const url = "https://restcountries.com/v2/all";
+
+const countriesCards = document.querySelector(".search__results--cards");
 
 //Fetch the data
 const LoadAPI = function () {
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayCountries(data));
+    .then((data) => data.forEach((data) => displayCountries(data)));
 };
 
-// function displayCountries()
+LoadAPI();
+
+function displayCountries(data) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("results--card");
+  newDiv.innerHTML = `
+    <img class="flag-img" src="${data.flag}" />
+      <div class="country-info">
+      <span class="country-name">${data.name}</span>
+      <ul class="country-properies">
+        <li class="property-box">
+          Population: <span class="property">${data.population}</span>
+        </li>
+        <li class="property-box">
+          Region: <span class="property">${data.region}</span>
+        </li>
+        <li class="property-box">
+          Capital: <span class="property">${data.capital}</span>
+        /li>
+      </ul>
+  `;
+  countriesCards.appendChild(newDiv);
+}
